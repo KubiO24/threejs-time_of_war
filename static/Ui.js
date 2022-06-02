c86975943693dc5845e553789ed20ffbdebbfcbb
Jakub Kowal <jkowal@dc514i.zsl>
2022-06-02 12:58:07 +0200

    Login screen

--- static/Ui.js
+++ static/Ui.js
@@ -1,11 +1,25 @@
 class Ui {
-    play = () => {
-        console.log('play')
-        
+    constructor() {
+        this.audioListener = window.addEventListener("mouseover", this.playMusic);
+    }
+
+    playMusic = () => {
         if(this.audio != undefined) {
             this.audio = new Audio('./music/loginTheme.mp3');
             this.audio.loop = true;
             this.audio.play();
+        };
+        window.removeEventListener("mouseover", this.playMusic)
+    }
+
+    login = () => {
+        let username = document.querySelector('#usernameInput').value;
+        if(username == '') {
+            document.querySelector('#emptyUsername').style.display = 'block';
+            return
         }
+        document.querySelector('#emptyUsername').style.display = 'none';
+
+        console.log(username)
     }
 }
